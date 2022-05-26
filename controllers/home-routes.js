2899833946621bad85076872db7377e77886af00
Gerard Mennella <gerardmennella21@gmail.com>
2022-05-26 19:46:33 -0400

    added routes to server

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,5 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection')
 
-router.use('/api', apiRoutes)
-router.use('/', homeRoutes)
-
-router.use((req, res) => {
-    res.status(404).end();
-})
 
 module.exports = router
