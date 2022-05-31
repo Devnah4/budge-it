8b08a6ef9a05838375285676024149978d146627
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-31 19:59:43 -0400

    Added the Dashboard route
    Created elements for dashboard and routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require('../config/connection')
 
 // Generates the homepage
 router.get("/", (req, res) => {
+    // Connects to the homepage handlebars file and generates it
     res.render('homepage');
 });
 
