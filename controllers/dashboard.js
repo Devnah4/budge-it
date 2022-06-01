28b3f96f1ca176b4e44a4c78501d2695cda79cca
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-31 20:25:16 -0400

    Added dashbord set up
    Added dashboard handlebars. added charts guidelines

---
+++ controllers/dashboard.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const withAuth = require('../utils/auth');
+const { User } = require('../models');
+
+router.get
